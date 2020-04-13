8aa9f54271dfc239ca8f3be37a6509c9f87d7aa0
pekkizen <60615893+pekkizen@users.noreply.github.com>
2020-04-13 17:25:07 +0300

    Add files via upload

--- xosh.go
+++ xosh.go
@@ -84,11 +84,18 @@ func (x Xosh) NextState() Xosh {
 	}
 }
 
+// Baseline256 is for benchmarking minimal 256-bit state generator.
+func (x *Xosh) Baseline256() uint64 {
+	next := x.s0
+	*x = Xosh{x.s3, x.s0, x.s1, x.s2}
+	return next
+}
+
 // Float64 returns a uniformly distributed pseudo-random float64 value in [0, 1).
 // Float64 uses 53 high bits of xoshiro256+
 func (x *Xosh) Float64() (next float64) {
 
-	return float64(x.Xoshiro256plus() >> 11) / (1 << 53)
+	return float64(x.Xoshiro256plus() >> 11) / (1<<53)
 }
 
 // State returns the current binary state of the generator x as []byte.
